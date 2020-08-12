40514157f8ceb0b3d9720ad9af2b637a4366a8cb
Baylua <37681112+Baylua@users.noreply.github.com>
2020-08-12 02:19:02 -0400

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,6 +3,6 @@ games { 'rdr3', 'gta5' }
 
 author 'Baylua#0001'
 description 'This sends a message telling a user through client-side to direct them to our Discord Server.'
-version '1.0.0'
+version '2.8.0'
 
 client_script './client/client.lua'
