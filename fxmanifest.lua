ecb93dbc7daae7a906d03b7ef79222f1d1526434
Baylua <37681112+Baylua@users.noreply.github.com>
2020-08-12 02:18:35 -0400

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version 'bodacious'
+games { 'rdr3', 'gta5' }
+
+author 'Baylua#0001'
+description 'This sends a message telling a user through client-side to direct them to our Discord Server.'
+version '1.0.0'
+
+client_script './client/client.lua'
